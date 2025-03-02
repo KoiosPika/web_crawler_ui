ec82acbbd9fcecad01ec9639453fbea6d5c45a50
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2025-03-02 15:28:32 -0800

    changed back to serveless

--- components/shared/MainPage.tsx
+++ components/shared/MainPage.tsx
@@ -5,6 +5,7 @@ import { ScrollArea } from "@radix-ui/react-scroll-area";
 import TreeGraph from "./TreeGraph";
 import { ScrollBar } from "../ui/scroll-area";
 import Image from "next/image";
+import { getCrawlData } from "@/lib/actions";
 
 export interface ILink {
     id: string;
@@ -48,18 +49,9 @@ export default function MainPage() {
 
     const handleCrawl = async () => {
         setLoading(true);
-        try {
-            const response = await fetch("http://13.58.34.251:5000/crawl", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ start_url: url, max_depth: depth }),
-            });
-            const data = await response.json();
-            console.log("Response:", data);
-            setData(data.crawled_pages);
-        } catch (error) {
-            console.error("Error:", error);
-        }
+        const crawledData = await getCrawlData(url, depth)
+        setData(crawledData)
+        console.log(data)
         setLoading(false);
     };
 
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,12 +1,13 @@
 "use server"
 
-import axios from "axios";
-
 export async function getCrawlData(url: string, depth: number) {
-    const response = await axios.post("http://13.58.34.251:5000/crawl", {
-        start_url: url,
-        max_depth: depth,
+    const response = await fetch("http://13.58.34.251:5000/crawl", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ start_url: url, max_depth: depth }),
     });
 
-    return response.data.crawled_pages
+    const data = await response.json();
+
+    return data.crawled_pages
 }
